63c38ee27135bae5d85794f6a5a1a8910666f067
Lotte Rombaut <lotte.rombaut2@gmail.com>
2018-09-26 12:43:05 +0200

    oefening 4

---
+++ Oef04.py
@@ -0,0 +1,21 @@
+getala = 16
+getalb = 20
+
+#decimaal
+print("Getal 1 is: ",getala)
+print("Getal 2 is: ",getalb)
+
+#naar octaal
+print((oct(getala)))
+print(str(oct(getalb)))
+
+#naar hexadecimaal
+print("Getal 1 in Hexadecimaal is: ",str(hex(getala)))
+print("Getal 2 in Hexadecimaal is:",str(hex(getalb)))
+
+#naar binair
+print(str(bin(getala)))
+print(str(bin(getalb)))
+
+#som
+print("De som is: ", getala + getalb)
