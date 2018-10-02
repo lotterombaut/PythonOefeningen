478d7300f5b9619e3e9586c34bdcaa1a9e592a06
Lotte Rombaut <lotte.rombaut2@gmail.com>
2018-10-02 18:13:48 +0200

    oefening 9

---
+++ 01_Introductie/OefeningCalender.py
@@ -0,0 +1,4 @@
+y = int(input("geef een jaar op: "))
+m = int(input("geef een maand op: "))
+
+print("Dit is de kalender: {0}".format(calendar.month(y,m)))
---
+++ 02_Controllerstructuren/Oef01.py
@@ -0,0 +1,14 @@
+#oefening 01 week 02
+#2 getallen vergelijken
+
+getal1 =  int(input("Geef een getal: "))
+getal2 = int(input("Geef een tweede getal: "))
+
+if (getal1 == getal2):
+        print("Beide getallen zijn dezelfde.")
+elif (getal1 < getal2):
+        print("Getal 1 is kleiner dan getal 2")
+else:
+    print("Getal 1 is groter dan getal 2")
+
+
