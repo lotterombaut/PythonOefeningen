149b81e6a9061f60cf520803110c7f40b2cdc716
Lotte Rombaut <lotte.rombaut2@gmail.com>
2018-10-02 18:26:15 +0200

    Commentaar aangepast in oefening 1 van week 2

--- 02_Controllerstructuren/Oef01.py
+++ 02_Controllerstructuren/Oef01.py
@@ -6,9 +6,10 @@ getal2 = int(input("Geef een tweede getal: "))
 
 if (getal1 == getal2):
         print("Beide getallen zijn dezelfde.")
-elif (getal1 < getal2):
-        print("Getal 1 is kleiner dan getal 2")
+#elif (getal1 < getal2):
+ #       print("Getal 1 is kleiner dan getal 2")
+#else:
+ #   print("Getal 1 is groter dan getal 2")
 else:
-    print("Getal 1 is groter dan getal 2")
-
+    print("De getallen zijn verschillend.")
 
