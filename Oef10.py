eabe9ae03d92c8eb9325d753e8989bde47e99dad
Lotte Rombaut <lotte.rombaut2@gmail.com>
2018-10-02 10:06:44 +0200

    oefening 10

---
+++ Oef10.py
@@ -0,0 +1,7 @@
+#oefening 10
+getal1 = int(input("Geef een getal op: "))
+getal2 = int(input("Geef een tweede getal in: "))
+
+uitkomst = (getal1 +getal2) * (getal1+getal2)
+
+print("({0} + {1})^2 = {2}".format(getal1,getal2,uitkomst))
