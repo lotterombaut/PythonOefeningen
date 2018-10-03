dc0c907dc3c8cb6db7ca9e16c15b30f3b5e68082
Lotte Rombaut <lotte.rombaut2@gmail.com>
2018-10-03 12:27:09 +0200

    oefeninge 11 en 12

---
+++ 02_Controllerstructuren/Oef11.py
@@ -0,0 +1,10 @@
+#oef11
+
+
+def getMax(nr1, nr2, nr3):
+ nr1 = int(input("geef een getal in: "))
+ nr2 = int(input("geef een getal in: "))
+ nr3 = int(input("geef een getal in: "))
+ return  max(nr1, nr2, nr3)
+
+print(getMax(0,0,0))
