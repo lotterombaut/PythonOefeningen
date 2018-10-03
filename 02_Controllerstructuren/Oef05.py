13ec97bb41c2f7985f4dc0e314348cc9eb1472fe
Lotte Rombaut <lotte.rombaut2@gmail.com>
2018-10-03 11:45:38 +0200

    Oefening 07

---
+++ 02_Controllerstructuren/Oef05.py
@@ -0,0 +1,8 @@
+#oefening 05
+
+from math import floor, ceil
+score = float(input("Hoeveel punten  behaalde je? "))
+
+print(round(score))
+decimal_part_of_score = score -int(score)
+if(decimal_part_of_score)
